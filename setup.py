871e61d8e762f39bc837dc4f3015046b3eed9f54
Vandana Singh <svandana56@gmail.com>
2025-08-16 00:41:13 +0530

    after adding e

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages , setup
+
+def get_requirements()->list[str]:
+    requirements_list = list[str] = []
+    return requirements_list
+
+
+setup(
+    
+    name='Sales',
+    version='0.0.1',
+    author='vandana',
+    author_email='svandana56@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements()
+    
+    
+    
+    
+)
