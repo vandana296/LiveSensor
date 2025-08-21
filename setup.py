bcc9af978b905ff2ce0033ce621b203cb0f732b9
Vandana Singh <svandana56@gmail.com>
2025-08-22 00:25:10 +0530

    after adding e

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 from setuptools import setup, find_packages
 
 def get_requirements()->list[str]:
-    requirements_list = list[str] = []
+    requirements_list=list[str]=[]
     return requirements_list
 
 
 
-
-
-
-
-
-
 setup(
-    name="LiveSensor", 
-    version="0.1.0",    
-    author="Vandana Singh",
-    author_email="vandana@example.com",  
-    packages=find_packages(),  
-    install_requires = get_requirements(),
+    name="sensor",  
+    version="0.1.0",
+    author="vandana",
+    author_email="svandana56@gmail.com",
+    packages=find_packages(),
+    install_requires=["pymongo"]
     
-    
+        
 )
